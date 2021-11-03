0e308b72dd2b0d5cd001ca968c9376ac92afb619
Rex Lundstrom <rexlundstrom@gmail.com>
2021-11-03 12:41:31 -0400

    functionality is questionable. Displaying in inner HTML instead of console, but losing some data

--- app.js
+++ app.js
@@ -8,7 +8,15 @@ button.addEventListener('click', function(){
     fetch('https://api.openweathermap.org/data/2.5/weather?q=' + inputValue.value + 
 '&appid=eb109a6b779c6397240cb7e5e6748fa1')
   .then(response => response.json())
-  .then(data => console.log(data))
+  .then(data => {
+      const nameValue = data['name'];
+      const tempValue = data['main']['temp'];
+      const descValue = data['weather'][0]['description'];
+
+      name.innerHTML = nameValue;
+      temp.innerHTML = tempValue;
+      description.innerHTML = descValue;
+  })
 
   .catch(err => alert("Error: enter city name properly"))
 })
